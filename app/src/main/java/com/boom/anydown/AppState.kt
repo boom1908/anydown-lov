60f1265d04ac9508bfae884c017fe81eba19f9d6
boom1908 <boomgonza1908@gmail.com>
2026-08-30 03:11:09 +0530

    Delete app/src/main/java/com/boom/anydown/AppState.kt

--- app/src/main/java/com/boom/anydown/AppState.kt
+++
@@ -1,7 +0,0 @@
-package com.boom.anydown
-
-sealed class AnydownState {
-    object Idle : AnydownState()
-    data class Loading(val progress: Float) : AnydownState()
-    data class Results(val title: String, val channel: String, val duration: String, val thumbnailUrl: String = "") : AnydownState()
-}
